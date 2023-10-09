b1a43fc095bc45bfc6ce94a9614ab20d3715a31d
Alexandr Aidarov <aidarovi7ia@mail.ru>
2023-10-09 17:33:31 +0300

    LessonThree

---
+++ LessonThree/CalculatorView.java
@@ -0,0 +1,10 @@
+package LessonThree;
+
+import java.util.Observer;
+
+// Интерфейс для View
+interface CalculatorView {
+    void setObserver(ViewObserver observer);
+    void updateResult(double result);
+    void clearScreen();
+}
