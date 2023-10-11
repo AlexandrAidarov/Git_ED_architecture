6b629bd2536159956a761b72ceadd03ea6e9d45e
Alexandr Aidarov <aidarovi7ia@mail.ru>
2023-10-11 19:49:39 +0300

    Task4

---
+++ LessonFour/Ticket.java
@@ -0,0 +1,10 @@
+package LessonFour;
+
+import java.util.Date;
+
+// Абстрактный класс и наследование
+abstract class Ticket implements TicketActions {
+    String routeId;
+    Date departureTime;
+    double price;
+}
---
+++ LessonFour/TicketActions.java
@@ -0,0 +1,10 @@
+package LessonFour;
+
+interface TicketActions {
+    boolean reserveTicket();
+    boolean deductFunds();
+    boolean confirmPurchase();
+    void updateTicketAvailability();
+    boolean cancelReservation();
+}
+
