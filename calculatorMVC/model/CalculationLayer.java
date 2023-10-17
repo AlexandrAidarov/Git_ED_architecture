016ea321b2066e3031596243c3b667345a288257
Alexandr Aidarov <aidarovi7ia@mail.ru>
2023-10-17 19:51:14 +0300

    LessonFive

---
+++ calculatorMVC/model/CalculationLayer.java
@@ -0,0 +1,27 @@
+package calculatorMVC.model;
+
+
+import calculatorMVC.model.substractOperation.SubstractOperation;
+import calculatorMVC.model.sumOperation.SumOperation;
+
+public class CalculationLayer {
+    public double add(double a, double b) {
+        SumOperation sumOperation = new SumOperation(a,b);
+        return sumOperation.sum();
+    }
+
+    public double subtract(double a, double b) {
+        SubstractOperation substractOperation = new SubstractOperation(a,b);
+        return substractOperation.substract();
+    }
+
+    public double multiply(double a, double b) {
+        // Умножение
+        return 1.0;
+    }
+
+    public double divide(double a, double b) {
+        // Деление
+        return 1.0;
+    }
+}
---
+++ calculatorMVC/view/OutputSlice.java
@@ -0,0 +1,8 @@
+package calculatorMVC.view;
+
+public class OutputSlice {
+    void displayOutput(String result) {
+        // Вывод результата на экран
+    }
+}
+
